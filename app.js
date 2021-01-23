e4e13ed4453eccfe5e99405a08d6ce4cf3127157
mBaenaCh <mateo.baena@hotmail.com>
2021-01-23 11:57:26 -0500

    Just adding some comments to the code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const codeBreaker = require('./codeBreaker');
 var express = require("express");
 var app = express();
 
-//1st endpoint
+//1st endpoint: Solve a codebreaker with a given number
 app.get('/solver/:numero', function (req, res) {
     var numero = req.params.numero;
     let result = codeBreaker.solveCode(numero);
@@ -11,7 +11,7 @@ app.get('/solver/:numero', function (req, res) {
     res.send(result);
 });
 
-//2nd endpoint
+//2nd endpoint: Set the secret before a codebreaker solve tryout
 app.get('/setSecret/:secret', function(req,res){
     let numero = req.params.secret;
     codeBreaker.setSecret(numero)
